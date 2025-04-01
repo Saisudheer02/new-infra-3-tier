759e3844594aee83d2b7ebc438955f43b31a8ff3
Saisudheer02 <nandikollasaisudheer@gmail.com>
2025-04-01 16:03:46 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
-# Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraform"
-
-  # Enable versioning
-  versioning {
-    enabled = true
-  }
+  bucket = "swiggy-buckefuvghvhgc11"
 }
 
+resource "aws_s3_bucket_versioning" "example_versioning" {
+  bucket = aws_s3_bucket.example_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
