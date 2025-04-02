beb1be46c47db886d3be0791e7ab402f0ddbb8e0
Saisudheer02 <nandikollasaisudheer@gmail.com>
2025-04-02 15:21:35 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-buckefuvghvhgc4874"
+  bucket = "swiggy-buckefuvghvhgc112345"
 }
 
 resource "aws_s3_bucket_versioning" "example_versioning" {
